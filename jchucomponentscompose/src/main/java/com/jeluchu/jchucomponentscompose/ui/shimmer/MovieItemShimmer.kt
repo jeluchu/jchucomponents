3e4fc88e27e8702dc2ed4c0db8155cc09f7b9008
Jesús María <infojeluchu@gmail.com>
2021-09-15 12:28:27 +0200

    Includes new ListViews, Shimmers and update core

---
+++ jchucomponentscompose/src/main/java/com/jeluchu/jchucomponentscompose/ui/shimmer/MovieItemShimmer.kt
@@ -0,0 +1,45 @@
+package com.jeluchu.jchucomponentscompose.ui.shimmer
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun MovieItemShimmer(
+    lists: List<Color>,
+    floatAnim: Float = 0f,
+    isVertical: Boolean
+) {
+
+    val brush = if (isVertical) Brush.verticalGradient(lists, 0f, floatAnim) else
+        Brush.horizontalGradient(lists, 0f, floatAnim)
+
+    Column(
+        modifier = Modifier
+            .padding(6.dp)
+            .width(130.dp)
+    ) {
+        Spacer(
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(180.dp)
+                .clip(RoundedCornerShape(10.dp))
+                .background(brush = brush)
+        )
+        Spacer(
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(30.dp)
+                .padding(top = 6.dp)
+                .clip(RoundedCornerShape(10.dp))
+                .background(brush = brush)
+        )
+    }
+
+}
