d991e6b2d998454ab40de51270682836b64e6492
Jesús María <infojeluchu@gmail.com>
2022-03-11 21:09:47 +0100

    Added nullable to Failures

--- jchucomponentscompose/src/main/java/com/jeluchu/jchucomponentscompose/core/exception/Failure.kt
+++ jchucomponentscompose/src/main/java/com/jeluchu/jchucomponentscompose/core/exception/Failure.kt
@@ -1,10 +1,7 @@
 package com.jeluchu.jchucomponentscompose.core.exception
 
-import com.jeluchu.jchucomponentscompose.core.extensions.ints.empty
-import com.jeluchu.jchucomponentscompose.core.extensions.strings.empty
-
 sealed class Failure {
-    data class NetworkConnection(val errorCode: Int? = null, val errorMessage: String) : Failure()
-    data class ServerError(val errorCode: Int? = Int.empty(), val errorMessage: String? = String.empty()) : Failure()
+    data class NetworkConnection(val errorCode: Int? = null, val errorMessage: String? = null) : Failure()
+    data class ServerError(val errorCode: Int? = null, val errorMessage: String? = null) : Failure()
     data class CustomError(val errorCode: Int, val errorMessage: String) : Failure()
 }
