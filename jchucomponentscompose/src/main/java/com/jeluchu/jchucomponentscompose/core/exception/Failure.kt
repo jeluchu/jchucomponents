a1f7d8a645e8c7910d7ca0c6930e7c16aabe1af3
Jesús María <infojeluchu@gmail.com>
2022-03-12 02:08:29 +0100

    Add Legacy case for Failure

--- jchucomponentscompose/src/main/java/com/jeluchu/jchucomponentscompose/core/exception/Failure.kt
+++ jchucomponentscompose/src/main/java/com/jeluchu/jchucomponentscompose/core/exception/Failure.kt
@@ -1,7 +1,8 @@
 package com.jeluchu.jchucomponentscompose.core.exception
 
 sealed class Failure {
-    data class NetworkConnection(val errorCode: Int? = null, val errorMessage: String? = null) : Failure()
-    data class ServerError(val errorCode: Int? = null, val errorMessage: String? = null) : Failure()
+    data class NetworkConnection(val errorCode: Int? = null, val errorMessage: String) : Failure()
+    data class ServerError(val errorCode: Int, val errorMessage: String) : Failure()
     data class CustomError(val errorCode: Int, val errorMessage: String) : Failure()
+    data class LegacyError(val errorCode: Int? = null, val errorMessage: String? = null) : Failure()
 }
