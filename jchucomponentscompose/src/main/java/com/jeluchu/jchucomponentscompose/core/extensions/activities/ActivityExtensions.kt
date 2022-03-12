a4314447aa9b132ef36bb41bee2e3d8eea08d9a6
Jesús María <infojeluchu@gmail.com>
2022-03-12 17:37:43 +0100

    Include Quickie Qr reader

--- jchucomponentscompose/src/main/java/com/jeluchu/jchucomponentscompose/core/extensions/activities/ActivityExtensions.kt
+++ jchucomponentscompose/src/main/java/com/jeluchu/jchucomponentscompose/core/extensions/activities/ActivityExtensions.kt
@@ -2,8 +2,15 @@ package com.jeluchu.jchucomponentscompose.core.extensions.activities
 
 import android.Manifest
 import android.app.Activity
+import android.content.ActivityNotFoundException
+import android.content.Intent
 import android.content.pm.PackageManager
+import android.net.Uri
+import androidx.activity.result.ActivityResultLauncher
+import androidx.annotation.StringRes
 import androidx.core.app.ActivityCompat
+import io.github.g00fy2.quickie.config.BarcodeFormat
+import io.github.g00fy2.quickie.config.ScannerConfig
 
 
 private val permissionsList =
@@ -21,3 +28,18 @@ val Activity.permissions: Unit
             )
         }
     }
+
+
+/** ---- ACTIVITY RESULT LAUNCHER -------------------------------------------------------------- **/
+
+fun ActivityResultLauncher<ScannerConfig>.openQRReader(@StringRes stringRes: Int) {
+    launch(
+        ScannerConfig.build {
+            setBarcodeFormats(listOf(BarcodeFormat.FORMAT_QR_CODE))
+            setOverlayStringRes(stringRes)
+            setHapticSuccessFeedback(false)
+            setShowTorchToggle(true)
+            setUseFrontCamera(false)
+        }
+    )
+}
